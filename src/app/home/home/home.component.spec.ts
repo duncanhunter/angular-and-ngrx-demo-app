b8032887414dad0a10038f21a57da8ffac3906d7
Duncan Hunter <duncan@duncanhunter.com.au>
2018-09-08 16:27:32 +1000

    Add home component test

--- src/app/home/home/home.component.spec.ts
+++ src/app/home/home/home.component.spec.ts
@@ -1,25 +1,34 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+describe('component: home', () => {
+  test('have a title of "The title"', () => {
+    const component = new HomeComponent();
+    expect(component.title).toEqual('The title');
   });
 });
+
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { HomeComponent } from './home.component';
+
+// describe('HomeComponent', () => {
+//   let component: HomeComponent;
+//   let fixture: ComponentFixture<HomeComponent>;
+
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ HomeComponent ]
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(HomeComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
