a3a3ecd3c081c0c03d763c9689eabd9649d40d54
Duncan Hunter <duncan@duncanhunter.com.au>
2018-09-08 21:39:51 +1000

    add event feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/containers/home/home.component';
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: '', pathMatch: 'full', redirectTo: 'home' },
       { path: 'home', component: HomeComponent },
+      { path: 'event', loadChildren: './event/event.module#EventModule' }
     ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
