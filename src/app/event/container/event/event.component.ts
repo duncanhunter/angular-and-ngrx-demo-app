ca835cbd1a26f11e188f1b73c0852ed96f66069e
Duncan Hunter <duncan@duncanhunter.com.au>
2018-09-10 21:12:06 +1000

    update variable for attendees

--- src/app/event/container/event/event.component.ts
+++ src/app/event/container/event/event.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Attendee } from '../../../models';
 
 @Component({
   selector: 'app-event',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event.component.scss']
 })
 export class EventComponent implements OnInit {
+  attendees: Attendee[];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  addAttendee(attendee: Attendee) {
+    this.attendees = [...this.attendees, attendee];
+    console.log('TCL: EventComponent -> addAttendee -> this.attendees', this.attendees);
   }
-
 }
