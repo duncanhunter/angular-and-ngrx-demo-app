cd53a4531f2ca73b5e7cad31aa674f2499e8b770
Duncan Hunter <duncan@duncanhunter.com.au>
2018-09-11 22:11:24 +1000

    fix service spec

--- src/app/event/services/event.service.spec.ts
+++ src/app/event/services/event.service.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { EventService } from './event.service';
 
 describe('EventService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [EventService]
     });
   });
